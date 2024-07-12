0bdddadb2524270bf1bb1029194dcfa213a26160
Francis Tsao <dialeleven@gmail.com>
2024-07-11 23:58:21 -0400

    Pass Data To a Component

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -19,12 +19,9 @@ export default {
 #app > div {
   border: dashed black 1px;
   display: inline-block;
+  width: 120px;
   margin: 10px;
   padding: 10px;
   background-color: lightgreen;
-}
-
-#app > div:hover {
-  cursor: pointer;
 }
 </style>
--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,31 +1,16 @@
 <template>
    <div v-on:click="countClicks">
-      <h2>{{ name }}</h2>
-      <p>{{ message }}</p>
-      <p id="red">You have clicked me {{ clicks }} times.</p>
+      <h2>{{ foodName }}</h2>
    </div>
 </template>
 
 <script>
 export default {
-   data() {
-      return {
-         name: 'Apples',
-         message: 'I like apples',
-         clicks: 0
-      }
-   },
-   methods: {
-      countClicks() {
-         this.clicks++;
-      }
-   }
+   props: [
+      'foodName'
+   ]
 }
 </script>
 
 <style>
-#red {
-    font-weight: bold ;
-    color: rgb(144, 12, 12);
-  }
 </style>
