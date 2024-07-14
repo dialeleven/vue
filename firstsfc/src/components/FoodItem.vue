d60d90375ba52d58e99aa56f7a7745d062a691ba
Francis Tsao <dialeleven@gmail.com>
2024-07-14 00:43:25 -0400

    comments

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -40,6 +40,19 @@ export default {
    },
    data() {
       return {
+         /*
+         The this keyword is used in Vue components to refer to the instance of the component.
+
+         In the data function, you need to initialize your local state (foodIsFavorite) with the
+         value of a prop (isFavorite). To do this, you must refer to the component instance 
+         to access the prop. Since props are part of the component instance, you 
+         use this.isFavorite to get the value of the isFavorite prop.
+
+         Example Without this
+         If you omit this, JavaScript will not understand that isFavorite is a prop of 
+         the Vue instance. Instead, it will look for a variable named isFavorite in the 
+         current scope, which doesn't exist, leading to a reference error.
+         */
          foodIsFavorite: this.isFavorite
       }
    },
