6e1c1c906d0b351b88fdcfe686d6221b318c129a
Francis Tsao <dialeleven@gmail.com>
2024-07-16 01:23:54 -0400

    renamed vue todo directory based off of the 'firstsfc' w3schools directory originally  used as a base to get started with our vue project

--- vue-todo-app/src/main.js
+++ vue-todo-app/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+//import FoodItem from './components/FoodItem.vue'
+import TodoList from './components/TodoList.vue'
+
+
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.component('todo-list', TodoList)
+app.mount('#app')
