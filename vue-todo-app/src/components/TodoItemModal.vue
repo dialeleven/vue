6721f3e5ced6508a35da1835cee91845a5636cc0
Francis Tsao <dialeleven@gmail.com>
2024-07-20 00:56:04 -0400

    setting up "add" todo method

--- vue-todo-app/src/components/TodoItemModal.vue
+++ vue-todo-app/src/components/TodoItemModal.vue
@@ -1,24 +1,24 @@
 <template>
-   <div className="modal-backdrop">
-      <div className="modal">
-         <div className="modal-header">
+   <div class="modal-backdrop">
+      <div class="modal">
+         <div class="modal-header">
             <h2>{{addEditMode}} Todo</h2>
-            <button className="close-button" @click="handleClose">×</button>
+            <button class="close-button" @click="handleClose">×</button>
          </div>
-         <div className="modal-body">
+         <div class="modal-body">
             <input
                type="text"
                v-model="todoTextInput"
                value={text}
             />
 
-            <div className="modal-date">
+            <div class="modal-date">
                <label>Due Date (Optional):</label>
                <input
                   type="datetime-local"
                   v-model="todoDueDateInput"
                   value={dateTime}
-                  className="modal-date-time"
+                  class="modal-date-time"
                />
             </div>
             <!-- 
@@ -32,24 +32,24 @@
                autoFocus
             />
 
-            <div className="modal-date">
+            <div class="modal-date">
                <label>Due Date (Optional):</label>
                <input
                   type="datetime-local"
                   value={dateTime}
-                  className="modal-date-time"
+                  class="modal-date-time"
                   onChange="{(e) => setDateTime(e.target.value)}
                   onKeyDown={handleKeyPress}"
                />
             </div>
              -->
          </div>
-         <div className="modal-footer">
-            <button className="modal-button" REACTonClick={handleSubmit}>{{addEditMode}}</button>
-            <button className="modal-button-close" @click="handleClose">Close</button>
+         <div class="modal-footer">
+            <button class="modal-button" @click="handleSubmit" REACTonClick={handleSubmit}>{{addEditMode}}</button>
+            <button class="modal-button-close" @click="handleClose">Close</button>
             <!-- {/* expression to conditionally render the 'New' button */} -->
             <!-- {
-               isNewButtonHidden ? null : <button className="modal-button" onClick={() => setIsNewButtonHidden(true)}>New</button>
+               isNewButtonHidden ? null : <button class="modal-button" onClick={() => setIsNewButtonHidden(true)}>New</button>
             } -->
          </div>
       </div>
@@ -101,7 +101,42 @@ export default {
 
       // user clicks the edit button to edit the todo item
       handleSubmit() {
-         //alert('hi');
+         // alert('handleSubmit, addEditMode: ' + this.addEditMode + ', text: ' + this.todoTextInput + ', dateTime: ' + this.todoDueDateInput);
+
+         switch (this.addEditMode) {
+            case 'Edit':
+               //alert('edit mode logic');
+               let formattedDateTime = ''; // set to empty string to prevent error
+
+               formattedDateTime = this.dateTime.replace("T", " ");
+               
+               updateTask(currentTaskId, text, formattedDateTime);
+               handleClose(); // Close the modal after updating
+               break;
+
+            default:
+               // if text is not empty, add the new task
+               if (this.todoTextInput !== null)
+               {
+                  // alert(this.todoDueDateInput);
+
+                  // if date/time is not null, replace the 'T' in the date/time with a space and set the date/time
+                  if (this.todoDueDateInput !== null)
+                  {
+                     // replace the 'T' in the date/time with a space and set the date/time
+                     const formattedDateTime = this.todoDueDateInput.replace("T", " ");
+                     
+                     this.todoDueDateInput = "";
+                  }
+                  // addTask({text, dateTime: formattedDateTime});       // Call the addTask function to add the new task
+                  this.todoTextInput = "";         // Reset the text state to an empty string
+                  this.handleClose();
+               }
+               else {
+                  alert('Please enter some text.');
+               }
+         }
+
          // console.log(this.task.id);
          // console.log(this.task.text);
          // console.log(this.task.duedate);
