ff65c20407913dc780bd16000e694d84cd2693d0
Francis Tsao <dialeleven@gmail.com>
2024-07-21 22:44:30 -0400

    adjusted App.vue to use PascalCase component name for consistency and added missing 'import' to <script> and changed to <script setup>. Old <script> syntax is commented out for reference.

--- vue-todo-app/src/App.vue
+++ vue-todo-app/src/App.vue
@@ -1,22 +1,34 @@
+<template>
+   <h1 class="app-header">VueTODO LIST</h1>
+   {{ message }}
+   <TodoList />
+</template>
+
+
+<script setup>
+import TodoList from './components/TodoList.vue';
+
+const message = '';
+</script>
+
+<!-- 
 <script>
+import TodoList from './components/TodoList.vue';
+
 export default {
    data() {
       return {
-         message: ''
+         // message: 'test'
       }
    },
-   methods: {
+
+   components: {
+      TodoList
    }
 }
 </script>
+-->
 
-<template>
-   <h1 className="app-header">VueTODO LIST</h1>
-   {{ message }}
-   <!-- this should be in kebab-case (e.g. '<todo-list />' instead of '<todoList />' because the naming
-    convention for components in templates is to use kebab-case) -->
-   <todo-list />
-</template>
 
 <style>
 </style>
